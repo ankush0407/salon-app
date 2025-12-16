2781519003acac7f73526580bfd4543118eb0df1
ankush0407 <inthebox.business@gmail.com>
2025-12-15 20:15:06 -0800

    Restore SalonOwnerApp.js to previous working version

--- client/src/components/SalonOwnerApp.js
+++ client/src/components/SalonOwnerApp.js
@@ -35,3 +35,39 @@ function SalonOwnerApp({ onLogout }) {
 }
 
 export default SalonOwnerApp;
+import SalonNavigation from './SalonNavigation';
+import Dashboard from './Dashboard';
+import OwnerPortal from './OwnerPortal';
+import Subscriptions from './Subscriptions';
+import Settings from './Settings';
+import './SalonOwnerApp.css';
+
+function SalonOwnerApp({ onLogout }) {
+  const [activeView, setActiveView] = useState('dashboard');
+
+  const renderContent = () => {
+    switch (activeView) {
+      case 'dashboard':
+        return <Dashboard onBack={() => {}} />;
+      case 'customers':
+        return <OwnerPortal onLogout={onLogout} setShowDashboard={() => setActiveView('dashboard')} />;
+      case 'subscriptions':
+        return <Subscriptions />;
+      case 'settings':
+        return <Settings />;
+      default:
+        return <Dashboard onBack={() => {}} />;
+    }
+  };
+
+  return (
+    <div className="salon-owner-app">
+      <SalonNavigation activeView={activeView} setActiveView={setActiveView} />
+      <main className="main-content">
+        {renderContent()}
+      </main>
+    </div>
+  );
+}
+
+export default SalonOwnerApp;
